7cb156e62a17eeb115abdbe1f260deb0e4c2cbee
DomoBox <eedomusbox@gmail.com>
2026-09-23 15:51:14 +0200

    Update footao-game-card.js

--- footao-game-card.js
+++ footao-game-card.js
@@ -1,8 +1,8 @@
 /* ========================================================
-   Footao Game Card  — v0.0.16
+   Footao Game Card  — v0.0.19
    ======================================================== */
 
-const FOOTAO_GAME_CARD_VERSION = "v0.0.16";
+const FOOTAO_GAME_CARD_VERSION = "v0.0.19";
 
 class FootaoGameCard extends HTMLElement {
 
